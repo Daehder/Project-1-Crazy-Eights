cb5bb2c94573b1f0645a1a3b21735d6741823590
Daehder <Erik@pcp075241pcs.wireless.calpoly.edu>
2014-10-13 14:40:05 -0700

    Card and Deck
    Two simple classes, card and deck, for handling cards in Crazy Eights

---
+++ Card.java
@@ -0,0 +1,97 @@
+/**
+ * A simple card object with a suit, value, and methods to get that info, compare to another card, 
+ * and get a human readable string of the card value and suit
+ * 
+ * @author Erik Miller
+ */
+ public class Card implements Comparable
+ {
+ 	private String suit;
+ 	private int value;
+ 	
+ 	/**
+ 	 * Constructs a card
+ 	 * 
+ 	 * @param suit	The suit of the card
+ 	 * @param value	The value of the card
+ 	 */
+ 	public Card(String suit, int value)
+ 	{
+ 		this.suit = suit;
+ 		this.value = value;
+ 	}
+ 	
+ 	/**
+ 	 * Returns the number value of the card
+ 	 * 
+ 	 * @return	The number value of the card
+ 	 */
+ 	public int getValue()
+ 	{
+ 		return value;
+ 	}
+
+ 	/**
+ 	 * Returns the suit of the card
+ 	 * 
+ 	 * @return	The suit of the card
+ 	 */
+	public String getSuit()
+	{
+		return suit;
+	}
+	
+	/**
+	 * Compares the value of this card to the card "other"
+	 * 
+	 * @param other The card to compare to
+	 * @return	returns positive if this is higher, negative if this is lower, and 0 if this is equal to other
+	 */
+	public int compareTo(Object other)
+	{
+		if(other instanceof Card)
+		{
+			if(this.value == 1 || ((Card)other).value == 1)
+		    if(this.value == 1 && this.value == 1)
+		        return 0;
+		    else if(this.value == 1)
+		        return 1;
+		    else
+		        return -1;
+	    	return this.value - ((Card)other).value;
+		}
+		return 0;
+	}
+	
+	/**
+	 * MODIFIED FOR CRAZY EIGHTS PROJECT: returns if the card has the same value or suit as the card "other"
+	 * 
+	 * @param other The card to compare to
+	 * @return 	Whether the card is equal in value or suit to the other
+	 */
+	public boolean equals(Card other)
+	{
+		return((this.value) == (other.value));
+	}
+
+	/**
+	 * Returns the card in a human readable string
+	 * 
+	 * @return A string with the value and suit of the card
+	 */
+ 	public String toString()
+ 	{
+ 		String output = null;
+ 		if(value == 1)
+ 			output = "Ace of " + suit;
+ 		else if(value > 1 && value < 11)
+ 			output = value + " of " + suit;
+ 		else if(value == 11)
+ 			output = "Jack of " + suit;
+ 		else if(value == 12)
+ 			output = "Queen of " + suit;
+ 		else if(value == 13)
+ 			output = "King of " + suit;
+ 		return output;
+ 	}
+ }
---
+++ Deck.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+/**
+ * A simple deck object with an array of cards and method to print, shuffle and deal from the deck
+ * 
+ * @author Erik
+ */
+public class Deck
+{
+    // Creates the array for the deck of cards
+	private Card[] deck;
+	private int top;
+
+	/**
+	 * Constructs a deck of cards, in order by suit then number
+	 */
+	public Deck()
+	{
+		deck = new Card[52];
+		for(int i = 1; i <= 52; i++)
+		{
+			String Suit = null;
+		    if(i >= 1 && i <= 13)
+		    {
+		    	Suit = "Clubs";
+			    Card a = new Card(Suit, i);
+			    deck[i - 1] = a;
+		    }
+		    else if(i >= 14 && i <= 26)
+		    {
+		    	Suit = "Diamonds";
+		        Card a = new Card(Suit, i - 13);
+		        deck[i - 1] = a;
+		    }
+		    else if(i >= 27 && i <= 39)
+		    {
+		        Suit = "Hearts";
+		        Card a = new Card(Suit, i - 26);
+		        deck[i - 1] = a;
+		    }
+		    else
+		    {
+		    	Suit = "Spades";
+                Card a = new Card(Suit, i - 39);
+                deck[i - 1] = a;
+		    }
+		}
+	}
+	
+	/**
+	 * Prints out the deck, starting at top
+	 */
+	public void print()
+	{
+	    for (int c = top; c < 52; c++)
+	    {
+	        System.out.println(deck[c]);
+	    }
+	    System.out.println();     //adds an extra line for readability
+	}
+	
+	/**
+	 * Shuffles the deck
+	 */
+	public void shuffle()
+	{
+	// uses a for look to go down the deck
+	    for(int c = 0; c < 52; c++)
+	    {
+	// picks a random number
+	        int cardToSwitch = (int)(Math.random() * 52);
+	// Store the cards at that original and random locations
+	        Card s = deck[cardToSwitch];
+            Card o = deck[c];
+	// Switches the random card with the card at the current location
+	        deck[cardToSwitch] = o;
+	        deck[c] = s;
+	    }
+	}
+	
+	/**
+	 * Picks the first card from the deck, removes if from the deck, increases top, and returns the card
+	 * 
+	 * @return
+	 */
+	public Card deal()
+	{
+	    Card r = deck[top];
+	    deck[top] = null;
+	    top++;
+	    return r;
+	}
+}
