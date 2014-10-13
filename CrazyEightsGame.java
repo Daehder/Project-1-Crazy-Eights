6eaa8d21fbec57df563fc1cc553368886c8c3d07
Daehder <Erik@pcp075241pcs.wireless.calpoly.edu>
2014-10-13 16:51:10 -0700

    Cleaning, comments, and endgame
    Added methods to determine when the game ends, cleaned up some code,
    and added some comments

--- CrazyEightsGame.java
+++ CrazyEightsGame.java
@@ -7,9 +7,9 @@ import java.util.*;
  */
 public class CrazyEightsGame {
 	private static final int HAND_SIZE = 8;
-	private Deck deck;
-	private Hand p1;
-	private Hand p2;
+	private Deck deck;	/** The deck that all the cards in the game come from */
+	private Hand p1;	/** Player 1's hand. The human player */
+	private Hand p2;	/** Player 2's hand. The computer player */
 	
 	public CrazyEightsGame() {
 		Scanner in = new Scanner(System.in);
@@ -28,7 +28,7 @@ public class CrazyEightsGame {
 				options();
 			else
 				System.out.println("Please enter a valid selection");
-		} while(status != 'q' || status == 'Q');
+		} while(status != 'q' || status != 'Q');
 	}
 
 	private void play() {
@@ -40,17 +40,32 @@ public class CrazyEightsGame {
 		deck.print();
 		
 		// Creates and fills the hands
-		p1 = new Hand();
-		p2 = new Hand();
+		p1 = new Hand(1);
+		p2 = new Hand(2);
 		fillHands(p1, p2);
 		
 		boolean done = false;
-		while(!done) {
-			System.out.println("Playing game");
-			done = true;
+		while(!gameOver()) {
+			
 		}
+		System.out.println("\nWould you like to play again?");
 	}
 	
+	/**
+	 * Checks to see if the deck ran out of cards or if one of the players won
+	 * 
+	 * @return Whether the gam is over
+	 */
+	private boolean gameOver() {
+		if(deck.empty())
+			System.out.println("The deck ran out of cards. It's a tie.\n");
+		else if(p1.size() == 0)
+			System.out.println("You got rid of all your cards! You Won!\n");
+		else if(p2.size() == 0)
+			System.out.println("The computer got rid of all it's cards. Better luck next time.\n");
+		return deck.empty() || p1.size() == 0 || p2.size() == 0;
+	}
+
 	/**
 	 * Fills both hands with HAND_SIZE number of cards
 	 * 
@@ -78,7 +93,7 @@ public class CrazyEightsGame {
 	 */
 	private void options() {
 		// TODO Print the options
-		// Ideas: Change the cheat key, implement more cheats, special rules,
+		// Ideas: Change the cheat key, implement more cheats, special rules, player names, 
 		//			(if we get graphics) themes
 	}
 
@@ -90,6 +105,5 @@ public class CrazyEightsGame {
 		System.out.println("\n(P)lay\n" +
 							"(R)ules\n" + 
 							"(O)ptions\n");
-	}
-	
+	}	
 }
