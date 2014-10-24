b6b6d8520af560bf1cd50821b79ed27b66bc3025
chanceofthat <chancesofthat@gmail.com>
2014-10-24 15:34:22 -0700

    ComputerHand
    Working on AI for Computer

--- ComputerHand.java
+++ ComputerHand.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,6 +7,11 @@ import java.util.Scanner;
  * @author Erik
  */
 public class ComputerHand extends Hand {
+	private ArrayList<Card> hand;
+	private ArrayList<Card> playableCards;
+	private ArrayList<Card> combination;
+	private ArrayList<Card> highestCombination;
+
 
 	public ComputerHand() {
 		super();
@@ -18,9 +24,59 @@ public class ComputerHand extends Hand {
 	public ComputerHand(String name) {
 		super(name);
 	}
+	
+	public void findPlayableCards(ArrayList<Card> hand, Card lastCard){
+		for(Card card: hand){
+			if(card.getSuitValue() == lastCard.getSuitValue() || card.getValue() == lastCard.getValue()){
+				playableCards.add(card);
+			}
+		}
+	}
 
 	@Override
 	public Card takeTurn(Card lastCard, Deck deck) {
+		findPlayableCards(hand, lastCard);
+		
+		//Add a parameter for max draw.
+		while(playableCards.isEmpty()){
+			hand.add(deck.Deal());
+			findPlayableCards(hand, lastCard);
+		}
+		
+		int play;
+		play = 1; //Change to random function
+		switch (play){
+		
+		//Play first
+		case 1: 
+			return playableCards.get(0);
+		// Play highest amount
+		case 2:
+			int highComboCount = 0;
+			for(Card testCard: playableCards){
+				int comboCount = 0;
+				combination.add(testCard);
+				for(Card card: playableCards){
+					if(card.getValue() == testCard.getValue() && !card.equals(testCard)){
+						combination.add(card);
+						comboCount++;
+					}
+				}
+				if(comboCount > highComboCount){
+					highComboCount = comboCount;
+					for(int i = 0; i < combination.size(); i++){
+						highestCombination.add(i, combination.remove(i));
+					}
+				}
+			}
+			findPlayableCards(highestCombination, lastCard);
+			return playableCards.get(0);
+			
+		default:
+			break;
+		}
+		
+		
 		
 		return lastCard;
 	}
